19d1d3038d020b2cd588bf0ff3e1801a3dcbedc6
MiguelPeralvo <miguel.peralvo@gmail.com>
2018-10-02 20:45:46 +0100

    Branch to test version without alchemy.

--- app.py
+++ app.py
@@ -10,15 +10,15 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 import connexion
-import orm
-db_session = None
+import db
+db_connection = None
 MAX_RETRIES = 1
 
 def get_historic_global_sentiments_inner(
         from_ms_ago=86400000, from_created_epoch_ms=1532441907000,
         limit=1000, downsample_freq=None, sample_rate=1.0, sentiment_type='all', retry_count=0
 ):
-    global db_session
+    global db_connection
     final_dataset = []
     delta_ts = datetime.utcnow() - datetime(1970, 1, 1)
     utc_now = int((delta_ts.days * 24 * 60 * 60 + delta_ts.seconds) * 1000 + delta_ts.microseconds / 1000.0)
@@ -41,14 +41,10 @@ def get_historic_global_sentiments_inner(
     else:
         sentiment_types = [sentiment_type]
 
+    sentiment_types_sql = "', '".join(sentiment_types)
+
     try:
         if downsample_freq:
-            # We'll sample using the created_at_epoch_ms field, so the sampling is consistent across invocations. Could introduce some bias.
-            # Useful for visualisation. Discouraged for analysis.
-            # orm.GlobalSentiment.created_at_epoch_ms >= starting_point,
-            # orm.GlobalSentiment.sentiment_type.in_(sentiment_types),
-
-            sentiment_types_sql = "', '".join(sentiment_types)
 
             sql_query = f"""
                 SELECT sentiment_type, sentiment_seconds_back, 
@@ -62,10 +58,11 @@ def get_historic_global_sentiments_inner(
                 WHERE created_at_epoch_ms >= {starting_point}
                 AND sentiment_type IN ('{sentiment_types_sql}')
                 GROUP BY sentiment_type, sentiment_seconds_back, bin
-                ORDER BY created_at_epoch_ms DESC             
+                ORDER BY created_at_epoch_ms DESC   
+                LIMIT {limit}
             """
 
-            results = db_session.connection().execute(sql_query)
+            results = db_connection.connection().execute(sql_query)
 
             final_dataset = [
                 {
@@ -77,37 +74,70 @@ def get_historic_global_sentiments_inner(
                     'min_created_at_epoch_ms': row[6],
                     'max_created_at_epoch_ms': row[7],
                 } for row in results
-            ][:limit]
+            ]
 
         else:
-            q = db_session.query(orm.GlobalSentiment)
-
-            q = q.filter(
-                orm.GlobalSentiment.created_at_epoch_ms >= starting_point,
-                orm.GlobalSentiment.sentiment_type.in_(sentiment_types),
-            )
-
-            q = q.order_by(orm.GlobalSentiment.created_at_epoch_ms.desc())
+            sql_query = f"""
+                SELECT sentiment_type, sentiment_seconds_back, 
+                created_at_epoch_ms,
+                sentiment_absolute,
+                sentiment_normalized,
+                created_at_epoch_ms AS min_created_at_epoch_ms,
+                created_at_epoch_ms AS max_created_at_epoch_ms
+                FROM analysis_global_sentiment 
+                WHERE created_at_epoch_ms >= {starting_point}
+                AND sentiment_type IN ('{sentiment_types_sql}')
+                ORDER BY created_at_epoch_ms DESC
+            """
 
             # The sampling is not random, we try to make the sample points equidistant in terms of points in the between.
             if sample_rate < 1:
+                results = db_connection.connection().execute(sql_query)
+
                 # pick_up_rate = (1/sample_rate).as_integer_ratio()
                 # skip_rate = int(sample_rate * 10000)
-                dataset = [(i, p.dump()) for i, p in enumerate(q)]
+                dataset = [(i, row) for i, row in enumerate(results)]
                 pre_limit_dataset = random.sample(dataset, int(sample_rate*len(dataset)))
                 # pre_limit_dataset = [p for i, p in enumerate(dataset) if (i % 10000) < skip_rate]
-                final_dataset = [value for index, value in sorted(pre_limit_dataset[:limit], key=lambda tup: tup[0])]
+
+                final_dataset = [
+                    {
+                        'sentiment_type': row[0],
+                        'sentiment_seconds_back': row[1],
+                        'created_at_epoch_ms': row[2],
+                        'sentiment_absolute': float(row[3]),
+                        'sentiment_normalized': row[4],
+                        'min_created_at_epoch_ms': row[5],
+                        'max_created_at_epoch_ms': row[6],
+                    } for _, row in sorted(pre_limit_dataset[:limit], key=lambda tup: tup[0])
+                ]
             else:
-                final_dataset = [p.dump() for p in q][:limit]
+                sql_query = f'''
+                    {sql_query}
+                    LIMIT {limit}
+                '''
+                results = db_connection.connection().execute(sql_query)
+
+                final_dataset = [
+                    {
+                        'sentiment_type': row[0],
+                        'sentiment_seconds_back': row[1],
+                        'created_at_epoch_ms': row[2],
+                        'sentiment_absolute': float(row[3]),
+                        'sentiment_normalized': row[4],
+                        'min_created_at_epoch_ms': row[5],
+                        'max_created_at_epoch_ms': row[6],
+                    } for row in results
+                ]
 
 
     except sqlalchemy.exc.OperationalError:
-        db_session.rollback()
-        db_session.remove()
+        db_connection.rollback()
+        db_connection.remove()
         logger.warning(f'Recreating session because of issue with previous session: {traceback.format_exc()}')
 
         # Restore the session and retry.
-        session_reconnect()
+        reconnect()
 
         if retry_count < MAX_RETRIES:
             final_dataset = get_historic_global_sentiments_inner(
@@ -116,10 +146,11 @@ def get_historic_global_sentiments_inner(
         else:
             raise
     except:
-        db_session.rollback()
+        db_connection.rollback()
         logger.error(f'An error occurred when managing the query for retrieving global sentiment: {traceback.format_exc()}')
         raise
 
+    logger.info(f"Most recent max_created_at_epoch_ms: {final_dataset[0]['max_created_at_epoch_ms']}")
     return final_dataset
 
 def get_historic_global_sentiments(
@@ -128,9 +159,9 @@ def get_historic_global_sentiments(
 ):
     return get_historic_global_sentiments_inner(from_ms_ago, from_created_epoch_ms, limit, downsample_freq, sample_rate, sentiment_type)
 
-def session_reconnect():
-    global db_session
-    db_session = orm.init_db(ssh, db_host, db_name, db_user, db_password, db_port, ssh_username, ssh_password, 'utf8mb4')
+def reconnect():
+    global db_connection
+    db_connection = db.connect_db(ssh, db_host, db_name, db_user, db_password, db_port, ssh_username, ssh_password, 'utf8mb4')
 
 
 db_host = os.getenv('AUTOMLPREDICTOR_DB_SERVER_IP', '127.0.0.1')
@@ -143,7 +174,7 @@ ssh_password = os.getenv('AUTOMLPREDICTOR_DB_SSH_PASSWORD')
 ssh = ssh_username is not None
 
 
-session_reconnect()
+reconnect()
 app = connexion.FlaskApp(__name__)
 app.add_api('swagger.yaml')
 
@@ -151,7 +182,10 @@ application = app.app
 
 @application.teardown_appcontext
 def shutdown_session(exception=None):
-    db_session.remove()
+
+    if db_connection and db_connection.is_connected():
+        db_connection.close()
+
 
 if __name__ == '__main__':
     app.run(port=8080)
---
+++ db.py
@@ -0,0 +1,65 @@
+# from sqlalchemy import Column, String, BigInteger, Numeric, create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import scoped_session, sessionmaker
+
+import sys
+sys.excepthook = sys.__excepthook__  # See https://groups.io/g/insync/topic/13778827?p=,,,20,0,0,0::recentpostdate%2Fsticky,,,20,2,0,13778827
+from sshtunnel import SSHTunnelForwarder
+# from sqlalchemy import create_engine
+import traceback
+import mysql.connector as sql
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+# Base = declarative_base()
+#
+# class GlobalSentiment(Base):
+#     __tablename__ = 'analysis_global_sentiment'
+#     sentiment_type = Column(String(30), primary_key=True)
+#     sentiment_seconds_back = Column(BigInteger(), primary_key=True)
+#     created_at_epoch_ms = Column(BigInteger(), primary_key=True)
+#     sentiment_absolute = Column(Numeric(precision=10, scale=3, asdecimal=True))
+#     sentiment_normalized = Column(Numeric(precision=5, scale=3, asdecimal=True), nullable=True)
+#
+#     def dump(self):
+#         return dict([(k, v) for k, v in vars(self).items() if not k.startswith('_')])
+#
+#
+# class GlobalSentimentSummary(GlobalSentiment):
+#     __tablename__ = 'analysis_global_sentiment'
+#     __table_args__ = {'extend_existing': True}
+#
+#     min_created_at_epoch_ms = Column(BigInteger(), primary_key=True, nullable=True)
+#     max_created_at_epoch_ms = Column(BigInteger(), primary_key=True, nullable=True)
+
+
+def connect_db(ssh, db_host, database_name, db_user, db_password, db_port, ssh_username, ssh_password, charset='utf8mb4'):
+
+    if ssh:
+        ssh_server = SSHTunnelForwarder(
+            ssh_address_or_host=(db_host, 22),
+            ssh_password=ssh_password,
+            ssh_username=ssh_username,
+            remote_bind_address=('127.0.0.1', db_port))
+
+        ssh_server.start()
+        local_bind_port = ssh_server.local_bind_port
+        final_db_port = local_bind_port
+        final_db_host = '127.0.0.1'
+    else:
+        final_db_port = db_port
+        final_db_host = db_host
+
+    conn = sql.connect(
+        host=final_db_host,
+        port=final_db_port,
+        user=db_user,
+        passwd=db_password,
+        db=database_name,
+        charset=charset
+    )
+
+    return conn
